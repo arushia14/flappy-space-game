8f9a1cc2d7896c384f0a4b9636d6a85366de928f
Arushi A <arushiagarwal14@gmail.com>
2025-05-10 09:19:59 -0400

    teeny tiny change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Flappy Space',
+  description: 'Created by Arushi Agarwal',
+  generator: 'Arushi Agarwal',
 }
 
 export default function RootLayout({
